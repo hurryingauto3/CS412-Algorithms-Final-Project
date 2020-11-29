ddec5dac3e6e3948d08a32919828bf6acffb67fb
Syed Anus Ali <49055757+anusali04928@users.noreply.github.com>
2020-11-30 03:58:39 +0500

    Code implementation

---
+++ Implementations/Pairwise-Exchange.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def cost_change(cost_mat, n1, n2, n3, n4):
+    return cost_mat[n1][n3] + cost_mat[n2][n4] - cost_mat[n1][n2] - cost_mat[n3][n4]
+
+
+def two_opt(route, cost_mat):
+    best = route
+    improved = True
+    while improved:
+        improved = False
+        for i in range(1, len(route) - 2):
+            for j in range(i + 1, len(route)):
+                if j - i == 1: continue
+                if cost_change(cost_mat, best[i - 1], best[i], best[j - 1], best[j]) < 0:
+                    best[i:j] = best[j - 1:i - 1:-1]
+                    improved = True
+        route = best
+    return best
