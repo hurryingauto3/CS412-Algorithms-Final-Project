d9adf4f5e6812873874050c5f3da4a5198ab80be
musaid <mr04302@st.habib.edu.pk>
2020-11-30 21:40:41 +0500

    basic algo complete

--- Implementations/Christodes-Serdyukov.py
+++ Implementations/Christodes-Serdyukov.py
@@ -4,15 +4,20 @@ from pygraph.mixins.labeling import *
 from collections import defaultdict
 import heapq
 
-G = graph()
+Graph = graph()
 
 nodes = [1,2,3,4,5]
-edges = [[(1,2), 4], [(1,4), 5], [(2,3), 6], [(2,5),3], [(1,3),2], [(3,5),2]]
+edges = [[(1,2), 4], [(1,4), 5], [(1,3),2], [(1,5),4], 
+    [(2,3), 6], [(2,5),3], 
+    [(3,5),2], [(3,4),7], 
+    [(4,5), 7], [(4,2), 3]]
 
-G.add_nodes(nodes)
+Graph.add_nodes(nodes)
 #print(G.edges())
 for edge in edges:
-     G.add_edge(edge[0], edge[1])
+     Graph.add_edge(edge[0], edge[1])
+
+#print(Graph.edges())
 
 def PrimMST(G: graph, v):
     mst = graph()
@@ -33,4 +38,30 @@ def PrimMST(G: graph, v):
     print(mst)
     return mst      
     
-lst = PrimMST(G, 1)
+min_tree = PrimMST(Graph, 1)
+
+def odd_vertices(T: graph):
+    odd_set = set()
+    for v in T:
+        if T.node_order(v) % 2 == 1:
+            odd_set.add(v)        
+    print(odd_set)
+    
+#odd_vertices(min_tree)
+
+#traversal()
+
+def induced_subgraph(G: graph, V: set):
+    pass
+
+def approx_TSP(G:graph, v):
+    min_tree = PrimMST(G, v)
+    tour = []
+    H = traversal(min_tree, v, 'pre')
+    for i in H:
+        tour.append(i)
+    tour.append(v)
+    print(tour)
+        
+
+approx_TSP(Graph, 1)
