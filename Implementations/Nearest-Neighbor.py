9c6bafda9b0c61dcbaee1b62b0dfe5f07a17cb22
alitaher021 <54749289+alitaher021@users.noreply.github.com>
2020-12-04 00:41:37 +0500

    Update Nearest-Neighbor.py

--- Implementations/Nearest-Neighbor.py
+++ Implementations/Nearest-Neighbor.py
@@ -1,1 +1,22 @@
+def NN(A, start):
+    """Nearest neighbor algorithm.
+    A is an NxN array indicating distance between N locations
+    start is the index of the starting location
+    Returns the path and cost of the found solution
+    """
+    path = [start]
+    cost = 0
+    N = A.shape[0]
+    mask = np.ones(N, dtype=bool)  # boolean values indicating which 
+                                   # locations have not been visited
+    mask[start] = False
 
+    for i in range(N-1):
+        last = path[-1]
+        next_ind = np.argmin(A[last][mask]) # find minimum of remaining locations
+        next_loc = np.arange(N)[mask][next_ind] # convert to original location
+        path.append(next_loc)
+        mask[next_loc] = False
+        cost += A[last, next_loc]
+
+    return path, cost
